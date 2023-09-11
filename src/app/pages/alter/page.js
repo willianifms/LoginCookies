cee1bc474c09684ce2ea4b105cb611d77db67ddb
willianifms <williancardoso00004@gmail.com>
2023-09-11 10:04:40 -0400

    create base form

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -1,10 +1,19 @@
 export default async function alter() {
-   return (
-       <div>
-           <h1>alter</h1>
-           <div>
-               teste rota
-           </div>
-       </div>
-   );
+
+
+  return (
+    <>
+      <h1>alter</h1>
+      <form action="">
+        <label htmlFor="">nome</label>
+        <input type="text" />
+
+        <label htmlFor="">E-mail</label>
+        <input type="email" />
+
+        <label htmlFor="">Senha</label>
+        <input type="password" name="pass" id="" />
+      </form>
+    </>
+  );
 };
--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -2,9 +2,18 @@ export default async function register() {
    return (
        <div>
            <h1>register</h1>
-           <div>
-               teste rota
-           </div>
+           <>
+           <form action="">
+        <label htmlFor="">nome</label>
+        <input type="text" />
+
+        <label htmlFor="">E-mail</label>
+        <input type="email" />
+
+        <label htmlFor="">Senha</label>
+        <input type="password" name="pass" id="" />
+      </form>
+           </>
        </div>
    );
 };
