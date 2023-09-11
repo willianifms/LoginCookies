476def69427570a84317eccd8f57ef1e90847e5c
willianifms <williancardoso00004@gmail.com>
2023-09-11 08:41:25 -0400

    create routes: register and alter

---
+++ src/app/pages/alter/page.js
@@ -0,0 +1,10 @@
+export default async function alter() {
+   return (
+       <div>
+           <h1>alter</h1>
+           <div>
+               teste rota
+           </div>
+       </div>
+   );
+};
---
+++ src/app/pages/register/page.js
@@ -0,0 +1,10 @@
+export default async function register() {
+   return (
+       <div>
+           <h1>register</h1>
+           <div>
+               teste rota
+           </div>
+       </div>
+   );
+};
