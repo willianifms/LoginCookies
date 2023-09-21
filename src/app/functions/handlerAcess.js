e647a4e24404632a9838bd1119afa667eb710b3b
oleovini <williancardoso00004@gmail.com>
2023-09-20 21:14:27 -0400

    comments

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -4,12 +4,12 @@ import { getUserAuthenticated } from "./handlerAcessAPI";
 
 const handlerAcessUser = async (user) => {
 
-    const userAuth = await getUserAuthenticated(user);
+    const userAuth = await getUserAuthenticated(user); // Chama a função getUserAuthenticated e espera por sua resolução
     
-    const isTokenValidate = validateToken(userAuth.token);
+    const isTokenValidate = validateToken(userAuth.token);// Chama a função validateToken e armazena o resultado
 
-    if (isTokenValidate) {
-        Cookies.set('token', userAuth.token, { expires: 1 });
+    if (isTokenValidate) {  // Verifica se o token é válido
+        Cookies.set('token', userAuth.token, { expires: 1 });   // Define um cookie com o valor do token e uma validade de 1 dia
         localStorage.setItem('name', userAuth.name);   // Armazene o nome do usuário na localStorage
     }
     
--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -6,19 +6,18 @@ const users = [
     { name: 'Mari', email: 'mari@email.com', password: 'lalala', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
     { name: 'Lucca', email: 'lucca@email.com', password: 'bomba', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' }
 ];
-const getUserAuthenticated = (user) => {
+const getUserAuthenticated = (user) => {//recebe um objeto 'user' como entrada e verifica se há um usuário com o mesmo email e senha na matriz 'users'. Se encontrar, retorna esse usuário.
     let userAuth = {}
     for (let i = 0; i < users.length; i++) {
-        if(users[i].email === user.email && users[i].password === user.password) 
-        {
-            userAuth=users[i];
+        if (users[i].email === user.email && users[i].password === user.password) {
+            userAuth = users[i];
         }
     }
 
     return userAuth;
 }
 
-const getUsers = (user) =>{
+const getUsers = (user) => {
 
     return users;
 }
